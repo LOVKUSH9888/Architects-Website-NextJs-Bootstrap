360931d2382f9ed7812a21c291d8eb503b36c925
LOVKUSH <lovkushyadav9888@gmail.com>
2023-09-19 16:28:25 +0530

    making the std project setting up the bootstrap in the next-js web app

---
+++ architects-website/src/app/page.js
@@ -0,0 +1,31 @@
+'use client'
+import Head from "next/head";
+import Script from "next/script";
+import Nav from "./Navbar/page.jsx";
+
+const page = () => {
+  return (
+    <>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+        crossOrigin="anonymous"
+      />
+    </Head>
+    {/* Adding Navbar */} 
+    <Nav/>
+    
+    <Script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+      crossOrigin="anonymous"
+    />
+   
+  </>
+  )
+}
+
+export default page
